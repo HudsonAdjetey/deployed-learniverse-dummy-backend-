3daef3936f51e3e70bda16a23d6cbe9bb45e14f1
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-26 07:25:00 -0700

    all files structure

---
+++ src/App.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect } from "react";
+import {
+  RouterProvider,
+  Outlet,
+  createBrowserRouter,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
+import Home from "./container/pages/Home";
+import Navbar from "./components/nav/Navbar";
+import Account from "./container/pages/Account";
+import Classes from "./container/pages/Classes";
+import Student from "./container/pages/Student";
+import Login from "./container/pages/Login";
+import SignUp from "./container/pages/SignUp";
+import ScheduleClass from "./container/pages/ScheduleClass";
+import Settings from "./container/pages/Settings";
+import { useSelector } from "react-redux";
+import Demo from "./container/pages/Demo";
+import Subscription from "./container/pages/Subscription";
+import AdminHome from "./container/pages/Home2";
+import Navbar2 from "./components/nav/Navbar2";
+import ErrorPage from "./container/pages/ErrorPage";
+import ErrorBoundary from "./container/pages/ErrorBoundary";
+import ToastContainers from "./components/toastify/ToastContainer";
+import Landing from "./container/pages/Landing";
+
+const App = () => {
+  const { user, loading } = useSelector((state) => state.user);
+
+  // check for admin User
+
+  const AuthGuard = ({ children }) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      const userFromLocalStorage = JSON.parse(localStorage.getItem("user"));
+      console.log("User from local storage:", userFromLocalStorage);
+
+      if (!user) {
+        <Navigate to={"/home"} />;
+      } else {
+        navigate("/");
+      }
+    }, [navigate, user, loading]);
+
+    return children;
+  };
+
+  const isAdmin = user && user.va1 === import.meta.env.VITE_VAL;
+  const Layout = () => {
+    const navigate = useNavigate();
+
+    return user ? (
+      <section className="container">
+        {isAdmin ? <Navbar /> : <Navbar2 />}
+        <Outlet />
+      </section>
+    ) : (
+      <Navigate to={"/home"} replace />
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: (
+        <AuthGuard>
+          <Login />
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "/signup",
+      element: <SignUp />,
+    },
+
+    {
+      path: "/",
+      element: (
+        <ErrorBoundary>
+          <Layout />
+        </ErrorBoundary>
+      ),
+      children: [
+        {
+          path: "/",
+          // element: <Home />,
+          element: isAdmin ? <AdminHome /> : <Home />,
+        },
+        {
+          path: "/account",
+          element: <Account />,
+        },
+        {
+          path: "/classes",
+          element: <Classes />,
+        },
+        {
+          path: "/student",
+          element: <Student />,
+        },
+        {
+          path: "/schedule-class",
+          element: <ScheduleClass />,
+        },
+        {
+          path: "/subscription",
+          element: <Subscription />,
+        },
+        {
+          path: "/settings",
+          element: <Settings />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <ErrorPage />,
+    },
+    {
+      path: "/home",
+      element: <Landing />,
+    },
+  ]);
+
+  return (
+    <>
+      <ToastContainers />
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
